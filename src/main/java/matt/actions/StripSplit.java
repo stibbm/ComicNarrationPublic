1fe1cbaa1514e1f73da660060fc0309897103714
Matthew Stibbins <matthewstibbins@gmail.com>
2025-03-06 15:22:49 -0800

    implement StripSplit action

--- src/main/java/matt/actions/StripSplit.java
+++ src/main/java/matt/actions/StripSplit.java
@@ -1,5 +1,17 @@
 package matt.actions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class StripSplit {
 
+    private PanelSplitterService panelSplitterService =
+        new PanelSplitterService();
+
+    public StripSplit() throws IOException {}
+
+    public List<Panel> run(List<Panel> panelList) throws IOException {
+        panelList = panelSplitterService.runWithPanelList(panelList);
+        return panelList;
+    }
 }
